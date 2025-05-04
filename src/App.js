527ed4b59761b9236625efeccbaaa127ccbd3f0a
Srish Bansal <srishbansal@Srishs-MacBook-Air.local>
2025-05-05 01:27:47 +0530

    Relative paths added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { books as initialBooks } from "./data/books";
-import BookCard from "./components/BookCard";
-import Header from "./components/Header";
-import SearchAndSort from "./components/SearchAndSort";
+import BookCard from "src/components/BookCard.js";
+import Header from ".src/components/Header.js";
+import SearchAndSort from "src/components/SearchAndSort.js";
 
 function App() {
   const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') === 'true');
