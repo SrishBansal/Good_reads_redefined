d1a8642dcf734efdf2e584eeb792f3f806fae64b
Srish Bansal <srishbansal@Srishs-MacBook-Air.local>
2025-05-05 11:20:03 +0530

    Fix import path for SearchAndSort

---
+++ build/precache-manifest.0b9fa050ad2c9c4b252c06cca29ddeab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4eef70adb67898803a06ed7b1c252c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ce2255e0543d6715862",
+    "url": "/static/css/main.121b56de.chunk.css"
+  },
+  {
+    "revision": "fc5168ef613818adb1a6",
+    "url": "/static/js/2.e6d0eda8.chunk.js"
+  },
+  {
+    "revision": "2ce2255e0543d6715862",
+    "url": "/static/js/main.9ab6086d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
